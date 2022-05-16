f9349058e577a481063418c344c9e46fb567ce44
SeonMun Choi <seonmun.choi@gmail.com>
2022-05-16 15:49:39 +0900

    Add useful math function

--- SDL Game Framework/SDL Game Framework/Macro.h
+++ SDL Game Framework/SDL Game Framework/Macro.h
@@ -1,5 +1,10 @@
 #pragma once
 
 #define SafeFree(p) { free(p); p = NULL; }
+
 #define LogWithErrorCode(msg, errorCode) SDL_Log("[%s:%d] %s : %d\n", __FILE__, __LINE__, msg, errorCode)
 #define LogInfo(fmt, ...) SDL_LogInfo(SDL_LOG_CATEGORY_APPLICATION, fmt, __VA_ARGS__)
+
+#define Min(lhs, rhs) ((lhs < rhs) ? (lhs) : (rhs))
+#define Max(lhs, rhs) ((lhs > rhs) ? (lhs) : (rhs))
+#define Clamp(minValue, value, maxValue) (Max(minValue, Min(value, maxValue)))
