61299b99d0843d910760b5016699623030ed89da
SeonMun Choi <seonmun.choi@gmail.com>
2022-05-16 13:54:16 +0900

    Add useful file

---
+++ SDL Game Framework/SDL Game Framework/Config.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define WINDOW_WIDTH	1280
+#define WINDOW_HEIGHT	720
+#define IMAGE_DIRECTORY	"Asset/Image"
+#define SOUND_DIRECTORY	"Asset/Sound"
---
+++ SDL Game Framework/SDL Game Framework/Framework.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "Framework/Image.h"
+#include "Framework/Input.h"
+#include "Framework/Random.h"
+#include "Framework/Renderer.h"
+#include "Framework/Timer.h"
