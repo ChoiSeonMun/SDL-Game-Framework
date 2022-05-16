b94e2ce7759d53324cec5c5b386f15293ab9fe44
SeonMun Choi <seonmun.choi@gmail.com>
2022-05-16 13:54:33 +0900

    Modify macro

--- SDL Game Framework/SDL Game Framework/Macro.h
+++ SDL Game Framework/SDL Game Framework/Macro.h
@@ -3,6 +3,3 @@
 #define SafeFree(p) { free(p); p = NULL; }
 #define LogWithErrorCode(msg, errorCode) SDL_Log("[%s:%d] %s : %d\n", __FILE__, __LINE__, msg, errorCode)
 #define LogInfo(fmt, ...) SDL_LogInfo(SDL_LOG_CATEGORY_APPLICATION, fmt, __VA_ARGS__)
-
-#define WINDOW_WIDTH	1280
-#define WINDOW_HEIGHT	720
