6574e787b7244e3bd128439800b3b06e38dffeac
csy-59 <csy010509@naver.com>
2022-05-18 14:15:54 +0900

    Feat: CSV 추가 버전_0518_csy

---
+++ SDL Game Framework/SDL Game Framework/Framework/Csv.h
@@ -0,0 +1,52 @@
+﻿#pragma once
+
+#include "Common.h"
+#include <wchar.h>
+
+#define MAXIMUM_ROW 200
+
+typedef struct CsvItem
+{
+	byte* RawData;
+} CsvItem;
+
+typedef struct CsvFile
+{
+	int32		ColumnCount;
+	int32		RowCount;
+	CsvItem* Items[MAXIMUM_ROW];
+} CsvFile;
+
+/// <summary>
+/// CSV ������ �м��� CsvFile ��ü�� �����.
+/// </summary>
+/// <param name="csvFile">CsvFile ��ü</param>
+/// <param name="filename">���� �̸�</param>
+void		CreateCsvFile(CsvFile* csvFile, const char* filename);
+
+/// <summary>
+/// CSV ��ü�� �ڿ��� �ݳ��Ѵ�.
+/// </summary>
+/// <param name="csvFile">CsvFile ��ü</param>
+void		FreeCsvFile(CsvFile* csvFile);
+
+/// <summary>
+/// ���ڵ带 ������ �����´�.
+/// </summary>
+/// <param name="item">���ڵ�</param>
+/// <returns></returns>
+int32		ParseToInt(const CsvItem item);
+
+/// <summary>
+/// ���ڵ带 ���ڿ��� �����´�.
+/// </summary>
+/// <param name="item">���ڵ�</param>
+/// <returns></returns>
+char* ParseToAscii(const CsvItem item);
+
+/// <summary>
+/// ���ڵ带 �����ڵ� ���ڿ��� �����´�.
+/// </summary>
+/// <param name="item">���ڵ�</param>
+/// <returns></returns>
+wchar_t* ParseToUnicode(const CsvItem item);
--- SDL Game Framework/SDL Game Framework/Framework/Scene.c
+++ SDL Game Framework/SDL Game Framework/Framework/Scene.c
@@ -553,31 +553,6 @@ void release_intro(void)
 #pragma endregion
 
 #pragma region MainScene
-const wchar_t* str2[] = {
-	L"���⼭�� ����� �̹��� �������� ���ؼ� �˾ƺ��ô�.",
-	L"ȭ��ǥŰ�� �̹����� �̵���ų �� �ֽ��ϴ�.",
-	L"EŰ�� ������ ����Ʈ�� �����ų �� �ֽ��ϴ�. ����Ʈ �Ҹ��� ������ ������ ���� �� �����ϼ���.",
-	L"MŰ�� ������ ���ų� �� �� �ֽ��ϴ�.",
-	L"PŰ�� ������ ���߰ų� �簳�� �� �ֽ��ϴ�.",
-	L"1���� 2������ ������ ������ �� �ֽ��ϴ�.",
-	L"WASD�� �̹����� �������� ������ �� �ֽ��ϴ�.",
-	L"KLŰ�� �̹����� �������� ������ �� �ֽ��ϴ�."
-};
-
-#define GUIDELINE_COUNT 8
-
-typedef struct MainSceneData
-{
-	Text		GuideLine[GUIDELINE_COUNT];
-	Music		BGM;
-	float		Volume;
-	SoundEffect Effect;
-	Image		BackGround;
-	float		Speed;
-	int32		X;
-	int32		Y;
-	int32		Alpha;
-} MainSceneData;
 
 void logOnFinished(void)
 {
@@ -629,166 +604,6 @@ void setScene(int32 indexNum, wchar_t* name, char* bgImageName,
 }
 */
 
-/*
-void init_main(void)
-{
-	setScenes();
-
-	g_Scene.Data = malloc(sizeof(MainSceneData));
-	memset(g_Scene.Data, 0, sizeof(MainSceneData));
-
-	MainSceneData* data = (MainSceneData*)g_Scene.Data;
-
-	for (int32 i = 0; i < GUIDELINE_COUNT; ++i)
-	{
-		Text_CreateText(&data->GuideLine[i], "d2coding.ttf", 16, str2[i], wcslen(str2[i]));
-	}
-
-	Image_LoadImage(&data->BackGround, "background.jfif");
-
-	Audio_LoadMusic(&data->BGM, "powerful.mp3");
-	Audio_HookMusicFinished(logOnFinished);
-	Audio_LoadSoundEffect(&data->Effect, "effect2.wav");
-	Audio_HookSoundEffectFinished(log2OnFinished);
-	Audio_PlayFadeIn(&data->BGM, INFINITY_LOOP, 3000);
-
-	data->Volume = 1.0f;
-
-	data->Speed = 400.0f;
-	data->X = 400;
-	data->Y = 400;
-	data->Alpha = 255;
-}
-
-void update_main(void)
-{
-	MainSceneData* data = (MainSceneData*)g_Scene.Data;
-
-	if (Input_GetKeyDown('E'))
-	{
-		Audio_PlaySoundEffect(&data->Effect, 1);
-	}
-
-	if (Input_GetKeyDown('M'))
-	{
-		if (Audio_IsMusicPlaying())
-		{
-			Audio_Stop();
-		}
-		else
-		{
-			Audio_Play(&data->BGM, INFINITY_LOOP);
-		}
-	}
-
-	if (Input_GetKeyDown('P'))
-	{
-		if (Audio_IsMusicPaused())
-		{
-			Audio_Resume();
-		}
-		else
-		{
-			Audio_Pause();
-		}
-	}
-
-	if (Input_GetKey('1'))
-	{
-		data->Volume -= 0.01f;
-		Audio_SetVolume(data->Volume);
-	}
-
-	if (Input_GetKey('2'))
-	{
-		data->Volume += 0.01f;
-		Audio_SetVolume(data->Volume);
-	}
-
-	if (Input_GetKey(VK_DOWN))
-	{
-		data->Y += data->Speed * Timer_GetDeltaTime();
-	}
-
-	if (Input_GetKey(VK_UP))
-	{
-		data->Y -= data->Speed * Timer_GetDeltaTime();
-	}
-
-	if (Input_GetKey(VK_LEFT))
-	{
-		data->X -= data->Speed * Timer_GetDeltaTime();
-	}
-
-	if (Input_GetKey(VK_RIGHT))
-	{
-		data->X += data->Speed * Timer_GetDeltaTime();
-	}
-
-	if (Input_GetKey('W'))
-	{
-		data->BackGround.ScaleY -= 0.05f;
-	}
-
-	if (Input_GetKey('S'))
-	{
-		data->BackGround.ScaleY += 0.05f;
-	}
-
-	if (Input_GetKey('A'))
-	{
-		data->BackGround.ScaleX -= 0.05f;
-	}
-
-	if (Input_GetKey('D'))
-	{
-		data->BackGround.ScaleX += 0.05f;
-	}
-
-	if (Input_GetKey('K'))
-	{
-		data->Alpha = Clamp(0, data->Alpha - 1, 255);
-		Image_SetAlphaValue(&data->BackGround, data->Alpha);
-	}
-
-	if (Input_GetKey('L'))
-	{
-		data->Alpha = Clamp(0, data->Alpha + 1, 255);
-		Image_SetAlphaValue(&data->BackGround, data->Alpha);
-	}
-
-	if (Input_GetKeyDown(VK_RETURN)) {
-		Scene_SetNextScene(SCENE_TEMP);
-	}
-}
-
-void render_main(void)
-{
-	MainSceneData* data = (MainSceneData*)g_Scene.Data;
-
-	for (int32 i = 0; i < GUIDELINE_COUNT; ++i)
-	{
-		SDL_Color color = { .a = 255 };
-		Renderer_DrawTextSolid(&data->GuideLine[i], 10, 20 * i, color);
-	}
-
-	Renderer_DrawImage(&data->BackGround, data->X, data->Y);
-}
-
-void release_main(void)
-{
-	MainSceneData* data = (MainSceneData*)g_Scene.Data;
-
-	for (int32 i = 0; i < GUIDELINE_COUNT; ++i)
-	{
-		Text_FreeText(&data->GuideLine[i]);
-	}
-	Audio_FreeMusic(&data->BGM);
-	Audio_FreeSoundEffect(&data->Effect);
-
-	SafeFree(g_Scene.Data);
-}
-*/
 #pragma endregion
 
 #pragma region TempScene
@@ -818,7 +633,7 @@ void init_main(void)
 	data->CurrentOptionNumber = 0;
 	data->CurrentTextNumber = 0;
 	data->BlackOutAlpha = 255;
-	Image_LoadImage(&data->BlackOutImage, "black.jpg");
+	Image_LoadImage(&data->BlackOutImage, "black.png");
 	Image_SetAlphaValue(&data->BlackOutImage, data->BlackOutAlpha);
 	Audio_PlayFadeIn(&data->Scene->BGM, INFINITY_LOOP, 2000);
 
