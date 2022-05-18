53374980db602b555b61c66e0d5a884c3f1e84a5
csy-59 <csy010509@naver.com>
2022-05-18 11:12:05 +0900

    Feat: merge 정리, 인트로에서 메인으로 넘어가는 부분 수정_0518_csy

--- SDL Game Framework/SDL Game Framework/Framework/Scene.c
+++ SDL Game Framework/SDL Game Framework/Framework/Scene.c
@@ -1,4 +1,4 @@
-#include "stdafx.h"
+﻿#include "stdafx.h"
 #include "Scene.h"
 
 #include "Framework.h"
@@ -8,45 +8,103 @@ Scene g_Scene;
 static ESceneType s_nextScene = SCENE_NULL;
 
 typedef struct tagScene {
-	int32			Number;							//�� �ѹ�
-	const wchar_t*	Name;							//�� �̸�
-	Image			BGImage;						//���ȭ��
-	Music			BGM;							//��� ����
-	Image			AdditionImage;					//�߰� �̹���
-	int32			AddImage_X;						//�߰� �̹��� ��ġ
-	int32			AddImage_Y;						//�߰� �̹��� ��ġ
-	SoundEffect		EffectSound;					//ȿ����
-	int32			EffectSoundTiming;				//ȿ���� ǥ�� Ÿ�̹�
-	int32			DialogCount;					//�ؽ�Ʈ ����
-	Text			DialogList[10][10];				//�ؽ�Ʈ �迭
-	int32			OptionCount;					//�ɼ� ����
-	Image			OptionImagesList[6];			//�ɼ� �̹��� �迭
-	int32			NextSceneNumberList[6];			//�ɼ� ���ý� �Ѿ�� �� �ѹ�
-	int32			NextEndingSceneNumberList[6];	//���� ���� �������� ���
-	bool			isEndingScene;					//��������
+	int32			Number;							//씬 넘버
+	const wchar_t* Name;								//씬 이름
+	Image			BGImage;							//배경화면
+	Music			BGM;							//배경 음악
+	Image			AdditionImage;						//추가 이미지
+	int32			AddImage_X;						//추가 이미지 위치
+	int32			AddImage_Y;						//추가 이미지 위치
+	SoundEffect		EffectSound;						//효과음
+	int32			EffectSoundTiming;						//효과음 표현 타이밍
+	int32			DialogCount;						//텍스트 갯수
+	Text			DialogList[10][10];						//텍스트 배열
+	int32			OptionCount;						//옵션 갯수
+	Image			OptionImagesList[6];					//옵션 이미지 배열
+	int32			NextSceneNumberList[6];					//옵션 선택시 넘어가는 씬 넘버
+	int32			NextEndingSceneNumberList[6];				//다음 씬이 엔딩씬일 경우
+	bool			isEndingScene;						//엔딩인지
 } SceneStruct;
 
-#define SCENE_COUNT	118
+#define SCENE_COUNT	8
+
 SceneStruct Scenes[SCENE_COUNT];
+
+void setScenes(void) {
+	//Scene 1
+	Scenes[0].Number = 0;
+	Scenes[0].Name = L"";
+	Image_LoadImage(&Scenes[0].BGImage, "csv/roof/S#0.start.jpg");
+	Audio_LoadMusic(&Scenes[0].BGM, "csv/roof/B-play.mp3");
+	Image_LoadImage(&Scenes[0].AdditionImage, "NULL");
+	Scenes[0].AddImage_X = 0;
+	Scenes[0].AddImage_Y = 0;
+	Audio_LoadMusic(&Scenes[0].EffectSound, "NULL");
+	Scenes[0].EffectSoundTiming = NULL;
+
+	Scenes[0].DialogCount = 2;
+	Text_CreateText(&Scenes[0].DialogList[0][0], "d2coding.ttf", 50, L"2031년 5월 13일 오랜만에 여동생을 만나 ", wcslen(L"2031년 5월 13일 오랜만에 여동생을 만나 "));
+	Text_CreateText(&Scenes[0].DialogList[0][1], "d2coding.ttf", 50, L"용산 아이파크 몰에 왔다.", wcslen(L"용산 아이파크 몰에 왔다."));
+	Text_CreateText(&Scenes[0].DialogList[0][2], "d2coding.ttf", 50, L"", wcslen(L""));
+	Text_CreateText(&Scenes[0].DialogList[1][0], "d2coding.ttf", 50, L"이곳엔 여동생이 가장 좋아하는 ", wcslen(L"이곳엔 여동생이 가장 좋아하는 "));
+	Text_CreateText(&Scenes[0].DialogList[1][1], "d2coding.ttf", 50, L"키즈카페가 있기 때문이다.", wcslen(L"키즈카페가 있기 때문이다."));
+	Text_CreateText(&Scenes[0].DialogList[1][2], "d2coding.ttf", 50, L"", wcslen(L""));
+
+	Scenes[0].OptionCount = 0;
+	Scenes[0].NextSceneNumberList[0] = 1;
+
+
+	//Scene 2
+	Scenes[1].Number = 1;
+	Scenes[1].Name = L"";
+	Image_LoadImage(&Scenes[1].BGImage, "csv/roof/S#1.roof.jpg");
+	Audio_LoadMusic(&Scenes[1].BGM, "csv/roof/B-play.mp3");
+	Image_LoadImage(&Scenes[1].AdditionImage, "NULL");
+	Scenes[1].AddImage_X = 0;
+	Scenes[1].AddImage_Y = 0;
+	Audio_LoadMusic(&Scenes[1].EffectSound, "NULL");
+	Scenes[1].EffectSoundTiming = NULL;
+
+	Scenes[1].DialogCount = 2;
+	Text_CreateText(&Scenes[1].DialogList[0][0], "d2coding.ttf", 50, L"동생이 키즈카페에서 만난 친구들과 노는 것을 확인한 후,", wcslen(L"동생이 키즈카페에서 만난 친구들과 노는 것을 확인한 후,"));
+	Text_CreateText(&Scenes[1].DialogList[0][1], "d2coding.ttf", 50, L"", wcslen(L""));
+	Text_CreateText(&Scenes[1].DialogList[1][0], "d2coding.ttf", 50, L"담배를 피러", wcslen(L"담배를 피러"));
+	Text_CreateText(&Scenes[1].DialogList[1][1], "d2coding.ttf", 50, L"용산 아이파크 몰 옥상으로 올라갔다.", wcslen(L"용산 아이파크 몰 옥상으로 올라갔다."));
+	Text_CreateText(&Scenes[1].DialogList[1][2], "d2coding.ttf", 50, L"", wcslen(L""));
+
+	Scenes[1].OptionCount = 0;
+	Scenes[1].NextSceneNumberList[0] = 0;
+
+	//Scene 3
+	Scenes[2].Number = 2;
+	Scenes[2].Name = L"";
+	Image_LoadImage(&Scenes[2].BGImage, "csv/roof/S#1-2.jpg");
+	Audio_LoadMusic(&Scenes[2].BGM, "csv/roof/B-play.mp3");
+	Image_LoadImage(&Scenes[2].AdditionImage, "csv/roof/S#2.jpg");
+	Scenes[2].AddImage_X = 800;
+	Scenes[2].AddImage_Y = 600;
+	Audio_LoadMusic(&Scenes[2].EffectSound, "NULL");
+	Scenes[2].EffectSoundTiming = NULL;
+
+	Scenes[2].DialogCount = 6;
+	Text_CreateText(&Scenes[1].DialogList[0][0], "d2coding.ttf", 50, L"동생이 키즈카페에서 만난 친구들과 노는 것을 확인한 후,", wcslen(L"동생이 키즈카페에서 만난 친구들과 노는 것을 확인한 후,"));
+	Text_CreateText(&Scenes[1].DialogList[0][1], "d2coding.ttf", 50, L"", wcslen(L""));
+	Text_CreateText(&Scenes[1].DialogList[1][0], "d2coding.ttf", 50, L"담배를 피러", wcslen(L"담배를 피러"));
+	Text_CreateText(&Scenes[1].DialogList[1][1], "d2coding.ttf", 50, L"용산 아이파크 몰 옥상으로 올라갔다.", wcslen(L"용산 아이파크 몰 옥상으로 올라갔다."));
+	Text_CreateText(&Scenes[1].DialogList[1][2], "d2coding.ttf", 50, L"", wcslen(L""));
+
+	Scenes[2].OptionCount = 0;
+	Scenes[2].NextSceneNumberList[0] = 2;
+}
+
+//#define SCENE_COUNT	118
+//SceneStruct Scenes[SCENE_COUNT];
 
 #pragma region TitleScene
 
 #define SOLID 0
 #define SHADED 1
 #define BLENDED 2
-
-//const wchar_t* str[] = {
-//	L"PRESS THE ENTER",
-//	//L"BŰ�� ������ ��Ʈ�� ���� ���մϴ�.",
-//	//L"IŰ�� ������ ��Ʈ�� ���Ÿ�ü�� ���մϴ�.",
-//	//L"UŰ�� ������ �ؽ�Ʈ�� ������ ����ϴ�.",
-//	//L"SŰ�� ������ �ؽ�Ʈ�� ��Ҽ��� ����ϴ�.",
-//	//L"NŰ�� ������ �ٽ� ������� ���ƿɴϴ�.",
-//	//L"CŰ�� ������ ���� ��尡 �ٲ�ϴ�. (Solid -> Shaded -> Blended)",
-//	//L"1Ű�� ������ �ؽ�Ʈ�� �۾����ϴ�.",
-//	//L"2Ű�� ������ �ؽ�Ʈ�� Ŀ���ϴ�.",
-//	//L"�����̽� Ű�� ������ ���� ������ �Ѿ�ϴ�."
-//};
 
 typedef struct TitleSceneData
 {
@@ -74,6 +132,8 @@ void init_title(void)
 	//{
 	//	Text_CreateText(&data->GuideLine[i], "d2coding.ttf", 16, str[i], wcslen(str[i]));
 	//}
+
+	setScenes();
 
 	data->FontSize = 48;
 	Text_CreateText(&data->EnterText, "chosun.ttf", data->FontSize, L"--- PRESS THE ENTER ---", 24);
@@ -249,69 +309,69 @@ void release_credit(void)
 #pragma region Intro
 
 const wchar_t* intro1str[] = {
-	L"������ ���� '���� ��'�� �������� ���� ������� ���� ���� ���ѹα���, ",
-	L"���������� �������� '���� ���̷���'�� ��ġ�ϰ� ���� ����ȭ�� �����ߴ�.",
+	L"한차례 퍼진 '좀비 병'의 유행으로 국가 멸망위기 까지 갔던 대한민국은, ",
+	L"선진국들의 도움으로 '좀비 바이러스'를 퇴치하고 국가 정상화에 돌입했다.",
 	L" ",
-	L"�� �̻��� '����'���� ���� ������ ����� �ΰ� �� �� ���� �������",
-	L" ����� ���� TF�� <�ູ ������> �� �����ߴ�.",
+	L"더 이상의 '좀비병'으로 인한 나라의 멸망을 두고 볼 수 없던 대통령은",
+	L" 대통령 산하 TF팀 <행복 연구소> 를 설립했다.",
 };
 const wchar_t* intro2str[] = {
-	L"����� ���� TF�� <�ູ ������> ������ ",
-	L"���� �������� �ູ�� �Ⱥ��� å���� ���� ������ �ϰ� �ֽ��ϴ�.",
+	L"대통령 산하 TF팀 <행복 연구소> 에서는 ",
+	L"국민 여러분의 행복과 안보를 책임질 여러 연구를 하고 있습니다.",
 	L" ",
-	L"�����е��� �ǰ��� �ູ��",
-	L"������ ���̷����κ������ѵ帮�� ���Ͽ�",
+	L"여러분들의 건강과 행복을",
+	L"질병과 바이러스로부터지켜드리기 위하여",
 	L" ",
-	L"<�ູ ������> �� ���� ��� �Ʒ� ��÷�� ���� ���� ������ ����Ͽ�",
-	L"������ ���� �� ������ �����ϰ� �ֽ��ϴ�.",
+	L"<행복 연구소> 는 정부 기관 아래 최첨단 연구 장비와 지식을 겸비하여",
+	L"꾸준한 연구 및 개발을 진행하고 있습니다.",
 };
 const wchar_t* intro3str[] = {
-	L"������ ���, �� <�ູ ������> ��",
-	L"<���� ���̷����� �̿��� �뷮 ��� ����>�� �����ϴ� ���̾���,  ",
-	L"�ű⼭ ���а迡�� ������ �ΰ��� ����ü�� ",
-	L"�̿��� ���� �� ������ �ϰ� �־���. ����, �غ� �����̾���.",
+	L"하지만 사실, 이 <행복 연구소> 는",
+	L"<좀비 바이러스를 이용한 대량 살상 무기>를 개발하는 곳이었고,  ",
+	L"거기서 과학계에서 금지된 인간의 염색체를 ",
+	L"이용한 지능 쥐 실험을 하고 있었다. 물론, 극비 사항이었다.",
 	L" ",
-	L"�ΰ��� ����ü�� �������� �̽��Ͽ� ������ ���̰� ",
-	L"��÷�� ���� ���� ������ ����Ͽ� ",
-	L"������ ���� �� ������ �����ϰ� �ֽ��ϴ�. ",
+	L"인간의 염색체와 뇌세포를 이식하여 지능을 높이고 ",
+	L"최첨단 연구 장비와 지식을 겸비하여 ",
+	L"꾸준한 연구 및 개발을 진행하고 있습니다. ",
 };
 const wchar_t* intro4str[] = {
-	L"�� ����� �������� �ȵǴ� ���δ�, \"���� ���̷���\" ���  ",
-	L"���� �ڵ� 000 ���� �� �����ϰ� ���� ���Ǻ��� ���ȴ�.",
+	L"이 사실이 밝혀지면 안되는 정부는, \"좀비 바이러스\" 라고  ",
+	L"질병 코드 000 제로 로 명명하고 전파 주의보를 내렸다.",
 	L" ",
-	L"<�ູ ������>�� '���� ���̷��� �ǽ���'�� �Ű��ϸ�,",
-	L"����ݵ� �ֱ�� �Ͽ���. ",
-	L"'���� ���̷��� �ǽ���'�� ����ȭ�� ����Ǵ� �����̸� ",
-	L"������ ����� �ƴϱ� ������ ",
-	L"<�ູ ������>�� ���� ġ�Ḧ �ϸ�",
-	L"'������ �ΰ�ȭ'�� ��ġ�� �����ϴٰ� ���� �Ͽ���.",
+	L"<행복 연구소>에 '좀비 바이러스 의심자'를 신고하면,",
+	L"포상금도 주기로 하였다. ",
+	L"'좀비 바이러스 의심자'는 좀비화가 진행되는 상태이며 ",
+	L"완전한 좀비는 아니기 때문에 ",
+	L"<행복 연구소>로 가서 치료를 하면",
+	L"'완전한 인간화'로 완치가 가능하다고 선전 하였다.",
 	L" ",
-	L"�׷��� ������ ��� �ùε��� \"���� ���̷���\"  ������ ���̴� �������",
-	L"������ �Ű��Ͽ� ������� �޾Ҵ�.",
+	L"그렇기 때문에 모든 시민들은 \"좀비 바이러스\"  증상을 보이는 사람들을",
+	L"국가에 신고하여 포상금을 받았다.",
 };
 const wchar_t* intro5str[] = {
-	L"������ '���� ���̷��� �ǽ���' ������ ",
-	L"������ ��ī ���� ���� �����꿡 ������, �� ������ �̷��ߴ�.",
+	L"하지만 '좀비 바이러스 의심자' 괴담은 ",
+	L"유투버 렉카 등을 통해 유투브에 퍼졌고, 그 내용은 이러했다.",
 	L" ",
-	L"1) '���� ���̷��� �ǽ���' ����",
-	L"<�ູ ������>�� �� ���� ���� �� ����� ����. ",
-	L"2) ���� '���� ���̷��� �ǽ���'���� ������ ���δ�.",
-	L"3) �����鿡�� �� ����� �߼� �� �ݾ��� �ٽ� ȸ���Ѵٴ� ��������",
-	L"�� �ʾ� ���� �ݾ��� �ָ絿�Ǽ��� �޾ư���.",
+	L"1) '좀비 바이러스 의심자' 들이",
+	L"<행복 연구소>를 들어가 나온 것을 본 사람이 없다. ",
+	L"2) 실제 '좀비 바이러스 의심자'들은 연구에 쓰인다.",
+	L"3) 가족들에게 이 사실을 발설 시 금액을 다시 회수한다는 조건으로",
+	L"몇 십억 대의 금액을 주며동의서는 받아간다.",
 	L" ",
-	L"�� �����̾���.",
+	L"는 괴담이었다.",
 };
 const wchar_t* intro6str[] = {
-	L"�� '���� ��'�� ū ���ۿ��� �־��µ�,",
-	L"������ �������� '���� ��' ������ �� ������ ",
+	L"이 '지능 쥐'는 큰 부작용이 있었는데,",
+	L"연구소 직원들이 '지능 쥐' 실험을 할 때마다 ",
 	L" ",
-	L"���� ������ ���� ���Ͽ� �����ǿ� ",
-	L"K-POP �뷡�� ��� Ʋ�� ������ �����߰�,",
-	L"�� �������� �� '���� ��'�� ������",
-	L"K-POP �뷡�� ���� ���� �ߴ� ���̴�.",
+	L"뇌파 반응을 보기 위하여 연구실에 ",
+	L"K-POP 노래를 계속 틀고 연구를 진행했고,",
+	L"그 영향으로 이 '지능 쥐'는 때때로",
+	L"K-POP 노래에 맞춰 춤을 추는 것이다.",
 	L" ",
-	L"�� �㿡 ������, ���� ����� ����ȭ�� ����Ǹ�",
-	L"���� ���� �߰� ���� �屸�� �ָ����� �� ���ɼ��� �ִٰ� �Ѵ�.",
+	L"이 쥐에 물리면, 물린 사람도 좀비화가 진행되며",
+	L"가끔 춤을 추고 싶은 욕구에 휘말리게 될 가능성도 있다고 한다.",
 };
 
 
@@ -568,79 +628,8 @@ void setScene(int32 indexNum, wchar_t* name, char* bgImageName,
 	Scenes[indexNum-1].NextSceneNumberList[0] = 1;
 }
 */
-void setScenes(void) {
-	//Scene 1
-	Scenes[0].Number = 0;
-	Scenes[0].Name = L"";
-	Image_LoadImage(&Scenes[0].BGImage, "csv/roof/S#0.start.jpg");
-	Scenes[0].BGImage.ScaleY -= 0.065f;
-	Scenes[0].BGImage.ScaleX -= 0.065f;
-	Audio_LoadMusic(&Scenes[0].BGM, "csv/roof/powerful.mp3");
-	Image_LoadImage(&Scenes[0].AdditionImage, "NULL");
-	Scenes[0].AddImage_X = 0;
-	Scenes[0].AddImage_Y = 0;
-	Audio_LoadMusic(&Scenes[0].EffectSound, "NULL");
-	Scenes[0].EffectSoundTiming = NULL;
 
-	Scenes[0].DialogCount = 2;
-	Text_CreateText(&Scenes[0].DialogList[0][0], "d2coding.ttf", 50, L"2031�� 5�� 13�� �������� �������� ���� ", wcslen(L"2031�� 5�� 13�� �������� �������� ���� "));
-	Text_CreateText(&Scenes[0].DialogList[0][1], "d2coding.ttf", 50, L"��� ������ũ ���� �Դ�.", wcslen(L"��� ������ũ ���� �Դ�."));
-	Text_CreateText(&Scenes[0].DialogList[0][2], "d2coding.ttf", 50, L"", wcslen(L""));
-	Text_CreateText(&Scenes[0].DialogList[1][0], "d2coding.ttf", 50, L"�̰��� �������� ���� �����ϴ� ", wcslen(L"�̰��� �������� ���� �����ϴ� "));
-	Text_CreateText(&Scenes[0].DialogList[1][1], "d2coding.ttf", 50, L"Ű��ī�䰡 �ֱ� �����̴�.", wcslen(L"Ű��ī�䰡 �ֱ� �����̴�."));
-	Text_CreateText(&Scenes[0].DialogList[1][2], "d2coding.ttf", 50, L"", wcslen(L""));
-	
-	Scenes[0].OptionCount = 0;
-	Scenes[0].NextSceneNumberList[0] = 1;
-
-
-	//Scene 2
-	Scenes[1].Number = 1;
-	Scenes[1].Name = L"";
-	Image_LoadImage(&Scenes[1].BGImage, "csv/roof/S#1.roof.jpg");
-	Scenes[1].BGImage.ScaleY -= 0.065f;
-	Scenes[1].BGImage.ScaleX -= 0.065f;
-	Audio_LoadMusic(&Scenes[1].BGM, "powerful.mp3");
-	Image_LoadImage(&Scenes[1].AdditionImage, "NULL");
-	Scenes[1].AddImage_X = 0;
-	Scenes[1].AddImage_Y = 0;
-	Audio_LoadMusic(&Scenes[1].EffectSound, "NULL");
-	Scenes[1].EffectSoundTiming = NULL;
-
-	Scenes[1].DialogCount = 2;
-	Text_CreateText(&Scenes[1].DialogList[0][0], "d2coding.ttf", 50, L"������ Ű��ī�信�� ���� ģ����� ��� ���� Ȯ���� ��,", wcslen(L"������ Ű��ī�信�� ���� ģ����� ��� ���� Ȯ���� ��,"));
-	Text_CreateText(&Scenes[1].DialogList[0][1], "d2coding.ttf", 50, L"", wcslen(L""));
-	Text_CreateText(&Scenes[1].DialogList[1][0], "d2coding.ttf", 50, L"��踦 �Ƿ�", wcslen(L"��踦 �Ƿ�"));
-	Text_CreateText(&Scenes[1].DialogList[1][1], "d2coding.ttf", 50, L"��� ������ũ �� �������� �ö󰬴�.", wcslen(L"��� ������ũ �� �������� �ö󰬴�."));
-	Text_CreateText(&Scenes[1].DialogList[1][2], "d2coding.ttf", 50, L"", wcslen(L""));
-	
-	Scenes[1].OptionCount = 0;
-	Scenes[1].NextSceneNumberList[0] = 0;
-
-	//Scene 3
-	Scenes[2].Number = 2;
-	Scenes[2].Name = L"";
-	Image_LoadImage(&Scenes[2].BGImage, "csv/roof/S#1-2.jpg");
-	Scenes[2].BGImage.ScaleY -= 0.065f;
-	Scenes[2].BGImage.ScaleX -= 0.065f;
-	Audio_LoadMusic(&Scenes[2].BGM, "powerful.mp3");
-	Image_LoadImage(&Scenes[2].AdditionImage, "csv/roof/S#2.jpg");
-	Scenes[2].AddImage_X = 800;
-	Scenes[2].AddImage_Y = 600;
-	Audio_LoadMusic(&Scenes[2].EffectSound, "NULL");
-	Scenes[2].EffectSoundTiming = NULL;
-
-	Scenes[2].DialogCount = 6;
-	Text_CreateText(&Scenes[1].DialogList[0][0], "d2coding.ttf", 50, L"������ Ű��ī�信�� ���� ģ����� ��� ���� Ȯ���� ��,", wcslen(L"������ Ű��ī�信�� ���� ģ����� ��� ���� Ȯ���� ��,"));
-	Text_CreateText(&Scenes[1].DialogList[0][1], "d2coding.ttf", 50, L"", wcslen(L""));
-	Text_CreateText(&Scenes[1].DialogList[1][0], "d2coding.ttf", 50, L"��踦 �Ƿ�", wcslen(L"��踦 �Ƿ�"));
-	Text_CreateText(&Scenes[1].DialogList[1][1], "d2coding.ttf", 50, L"��� ������ũ �� �������� �ö󰬴�.", wcslen(L"��� ������ũ �� �������� �ö󰬴�."));
-	Text_CreateText(&Scenes[1].DialogList[1][2], "d2coding.ttf", 50, L"", wcslen(L""));
-	
-	Scenes[2].OptionCount = 0;
-	Scenes[2].NextSceneNumberList[0] = 2;
-}
-
+/*
 void init_main(void)
 {
 	setScenes();
@@ -799,6 +788,7 @@ void release_main(void)
 
 	SafeFree(g_Scene.Data);
 }
+*/
 #pragma endregion
 
 #pragma region TempScene
@@ -816,7 +806,7 @@ bool showOptions = false;
 Text* ShowText;
 int32 s_CurrentScene = 0;
 SDL_Color color = { 0,0,0,0 };
-void init_temp(void)
+void init_main(void)
 {
 	g_Scene.Data = malloc(sizeof(MainScene));
 	memset(g_Scene.Data, 0, sizeof(MainScene));
@@ -828,7 +818,7 @@ void init_temp(void)
 	data->CurrentOptionNumber = 0;
 	data->CurrentTextNumber = 0;
 	data->BlackOutAlpha = 255;
-	Image_LoadImage(&data->BlackOutImage, "black.png");
+	Image_LoadImage(&data->BlackOutImage, "black.jpg");
 	Image_SetAlphaValue(&data->BlackOutImage, data->BlackOutAlpha);
 	Audio_PlayFadeIn(&data->Scene->BGM, INFINITY_LOOP, 2000);
 
@@ -837,17 +827,17 @@ void init_temp(void)
 	ShowText = NULL;
 }
 
-void update_temp(void)
+void update_main(void)
 {
-	MainScene* data = (MainScene*) g_Scene.Data;
+	MainScene* data = (MainScene*)g_Scene.Data;
 
-	//���� ���� ���
+	//보통 씬일 경우
 	if (!isSceneChanging) {
 		if (data->BlackOutAlpha > 0) {
 			data->BlackOutAlpha -= 5;
 		}
 		else {
-			//Ű���� �� �Է�
+			//키보드 값 입력
 			if (Input_GetKeyDown(VK_SPACE)) {
 
 				if (data->CurrentTextNumber < data->Scene->DialogCount) {
@@ -862,7 +852,7 @@ void update_temp(void)
 			}
 		}
 
-		//�ؽ�Ʈ ǥ�ð� �ʿ��� ���
+		//텍스트 표시가 필요한 경우
 		if (data->CurrentTextNumber < data->Scene->DialogCount) {
 			int32 i = 0;
 			ShowText = data->Scene->DialogList[data->CurrentTextNumber];
@@ -870,11 +860,11 @@ void update_temp(void)
 				color.a += 5;
 			}
 		}
-		//�ɼ��� ���;��ϴ� ���
+		//옵션이 나와야하는 경우
 		else {
 			showOptions = true;
 
-			//������ ����
+			//선택지 선택
 			int32 optionCount = data->Scene->OptionCount;
 
 			Image_SetAlphaValue(&data->Scene->OptionImagesList[data->CurrentOptionNumber], 125);
@@ -899,7 +889,7 @@ void update_temp(void)
 				}
 			}
 
-			//�¿�Ű�� ����
+			//좌우키로 설정
 			if (Input_GetKeyDown(VK_LEFT)) {
 				if (data->CurrentOptionNumber > 0) {
 					data->CurrentOptionNumber--;
@@ -918,7 +908,7 @@ void update_temp(void)
 				}
 			}
 
-			//�� �Ʒ� Ű��
+			//위 아래 키로
 			if (Input_GetKeyDown(VK_UP)) {
 				if (data->Scene->OptionCount >= 3) {
 					if (data->CurrentOptionNumber >= 2) {
@@ -937,7 +927,7 @@ void update_temp(void)
 
 			Image_SetAlphaValue(&data->Scene->OptionImagesList[data->CurrentOptionNumber], 255);
 
-			//������ ����
+			//선택지 선택
 			if (Input_GetKeyDown(VK_RETURN)) {
 				isSceneChanging = true;
 				Audio_FadeOut(1800);
@@ -952,28 +942,28 @@ void update_temp(void)
 		else {
 			isSceneChanging = false;
 			s_CurrentScene = data->Scene->NextSceneNumberList[data->CurrentOptionNumber];
-			Scene_SetNextScene(SCENE_TEMP);
+			Scene_SetNextScene(SCENE_MAIN);
 		}
 	}
 	Image_SetAlphaValue(&data->BlackOutImage, data->BlackOutAlpha);
 }
 
-void render_temp(void)
+void render_main(void)
 {
 	MainScene* data = (MainScene*)g_Scene.Data;
 
-	//��� �̹��� ���
+	//배경 이미지 출력
 	Renderer_DrawImage(&data->Scene->BGImage, 0, 0);
 	//Renderer_DrawImage(&TextBGImage, 30, 30);
-	
-	//�ؽ�Ʈ ���
+
+	//텍스트 출력
 	int32 i = 0;
 	while (ShowText[i].Length != 0) {
 		Renderer_DrawTextSolid(&ShowText[i], 400, 300 + i * 80, color);
 		i++;
 	}
 
-	//������ ���
+	//선택지 출력
 	if (showOptions) {
 		if (data->Scene->OptionCount > 2) {
 			for (int i = 0; i < data->Scene->OptionCount; i++) {
@@ -987,17 +977,16 @@ void render_temp(void)
 		}
 	}
 
-	//���̵� �� ���̵� �ƿ� ȿ���� ���� ������ ���
+	//페이드 인 페이드 아웃 효과를 위한 검은색 배경
 	Renderer_DrawImage(&data->BlackOutImage, 0, 0);
 }
 
-void release_temp(void)
+void release_main(void)
 {
 	MainScene* data = (MainScene*)g_Scene.Data;
 
 	SafeFree(g_Scene.Data);
 }
-
 
 #pragma endregion
 
@@ -1056,12 +1045,12 @@ void Scene_Change(void)
 		g_Scene.Render = render_main;
 		g_Scene.Release = release_main;
 		break;
-	case SCENE_TEMP:
- 		g_Scene.Init = init_temp;
-		g_Scene.Update = update_temp;
-		g_Scene.Render = render_temp;
-		g_Scene.Release = release_temp;
-		break;
+	//case SCENE_TEMP:
+ 	//	g_Scene.Init = init_temp;
+	//	g_Scene.Update = update_temp;
+	//	g_Scene.Render = render_temp;
+	//	g_Scene.Release = release_temp;
+	//	break;
 	}
 
 	g_Scene.Init();
